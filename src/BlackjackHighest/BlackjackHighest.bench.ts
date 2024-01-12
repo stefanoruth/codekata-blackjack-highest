bfe54680b5919e03af674cbbf240d1c73f4d2b3a
Lagoni <andreastlagoni@gmail.com>
2024-01-12 13:02:44 +0100

    new

--- src/BlackjackHighest/BlackjackHighest.bench.ts
+++ src/BlackjackHighest/BlackjackHighest.bench.ts
@@ -1,8 +1,34 @@
 import { bench, describe } from 'vitest'
 import { blackjackHighest } from './BlackjackHighest'
+import { blackjackHighestNew } from './BlackjackHighest.new'
 
 describe('BlackjackHighest', () => {
-    bench('below king', () => {
+    bench('blackjack highest old', () => {
         blackjackHighest(['king', 'ace', 'two'])
+        blackjackHighest(['king', 'ace', 'two'])
+        blackjackHighest(['queen', 'ace', 'ten'])
+        blackjackHighest(['queen', 'ace', 'seven'])
+        blackjackHighest(['five', 'six', 'king'])
+        blackjackHighest(['five', 'eight', 'jack'])
+        blackjackHighest(['four', 'four', 'four', 'four', 'four', 'ace'])
+        blackjackHighest(['three', 'eight', 'queen'])
+        blackjackHighest(['four', 'seven', 'ace'])
+        blackjackHighest(['jack', 'ace'])
+        blackjackHighest(['two', 'three', 'four', 'five', 'six', 'ace'])
+        blackjackHighest(['ace', 'ace', 'four', 'five'])
+    })
+    bench('blackjack highest new', () => {
+        blackjackHighestNew(['king', 'ace', 'two'])
+        blackjackHighestNew(['king', 'ace', 'two'])
+        blackjackHighestNew(['queen', 'ace', 'ten'])
+        blackjackHighestNew(['queen', 'ace', 'seven'])
+        blackjackHighestNew(['five', 'six', 'king'])
+        blackjackHighestNew(['five', 'eight', 'jack'])
+        blackjackHighestNew(['four', 'four', 'four', 'four', 'four', 'ace'])
+        blackjackHighestNew(['three', 'eight', 'queen'])
+        blackjackHighestNew(['four', 'seven', 'ace'])
+        blackjackHighestNew(['jack', 'ace'])
+        blackjackHighestNew(['two', 'three', 'four', 'five', 'six', 'ace'])
+        blackjackHighestNew(['ace', 'ace', 'four', 'five'])
     })
 })
---
+++ src/BlackjackHighest/BlackjackHighest.new.ts
@@ -0,0 +1,108 @@
+export function blackjackHighestNew(cards: string[]) {
+    const numbers = convertStringsToNumbers(cards)
+    let myNumber = numbers.reduce((acc, current) => {
+        return (acc = acc + current.number)
+    }, 0)
+
+    let highestCard = numbers.reduce(function (prev, current) {
+        if (prev.number === current.number && current.sortNumber && prev.sortNumber) {
+            return prev.sortNumber > current.sortNumber ? prev : current
+        }
+        return prev && prev.number > current.number ? prev : current
+    })
+
+    const title = cards.reduce((acc, card) => {
+        if (card === 'ace') {
+            if (myNumber + 11 > 21) {
+                myNumber = myNumber + 1
+                acc = `below ${highestCard.value}`
+            } else {
+                highestCard = {
+                    number: 11,
+                    value: 'ace',
+                }
+                myNumber = myNumber + 11
+            }
+        }
+        if (myNumber === 21) {
+            return `blackjack ${highestCard.value}`
+        }
+        if (myNumber > 21) {
+            return `above ${highestCard.value}`
+        } else {
+            return `below ${highestCard.value}`
+        }
+    }, '')
+    return title
+}
+
+const convertStringsToNumbers = (cards: string[]) => {
+    const numbers = cards.reduce<
+        {
+            number: number
+            value: string
+            sortNumber?: number
+        }[]
+    >((acc, card) => {
+        if (card === 'ace') {
+            return acc
+        }
+        switch (card) {
+            case 'one': {
+                acc.push({ number: 1, value: card })
+                break
+            }
+            case 'two': {
+                acc.push({ number: 2, value: card })
+                break
+            }
+            case 'three': {
+                acc.push({ number: 3, value: card })
+                break
+            }
+            case 'four': {
+                acc.push({ number: 4, value: card })
+                break
+            }
+            case 'five': {
+                acc.push({ number: 5, value: card })
+                break
+            }
+            case 'six': {
+                acc.push({ number: 6, value: card })
+                break
+            }
+            case 'seven': {
+                acc.push({ number: 7, value: card })
+                break
+            }
+            case 'eight': {
+                acc.push({ number: 8, value: card })
+                break
+            }
+            case 'nine': {
+                acc.push({ number: 9, value: card })
+                break
+            }
+            case 'ten': {
+                acc.push({ number: 10, value: card, sortNumber: 1 })
+                break
+            }
+            case 'jack': {
+                acc.push({ number: 10, value: card, sortNumber: 2 })
+                break
+            }
+            case 'queen': {
+                acc.push({ number: 10, value: card, sortNumber: 3 })
+                break
+            }
+            case 'king': {
+                acc.push({ number: 10, value: card, sortNumber: 4 })
+                break
+            }
+        }
+        return acc
+    }, [])
+
+    return numbers
+}
